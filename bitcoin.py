cbeca12beca7f6256e043b86418dec8b8cc5bef1
Nkurikiyimana Aimable <aimablenkurikiyimana1@gmail.com>
2024-12-24 15:32:00 +0200

    more codes

--- bitcoin.py
+++ bitcoin.py
@@ -1,21 +1,28 @@
 import sys
 import requests
 
+
 def main():
     bitcoins = get_coins()
     usd = convert(bitcoins)
-    print(usd)
+    print(f"{usd:,.4f}")
 
 
 def convert(bitcoins):
-    # get current bitcoin value
-    response = requests.get('https://api.coindesk.com/v1/bpi/currentprice.json')
-    return "No price"
+    # geting current bitcoin value vi an api for the coinDesk Bitcoind Price Index
+    try:
+        response = requests.get("https://api.coindesk.com/v1/bpi/currentprice.json")
+    except requests.RequestException:
+        sys.exit("Couldn't get required data")
+    data = response.json()
+    # current bitcoin rate_float
+    rate_float = data["bpi"]["USD"]["rate_float"]
+    return bitcoins * rate_float
 
 
 def get_coins():
     try:
-        bitcoins = int(sys.argv[1])
+        bitcoins = float(sys.argv[1])
         return bitcoins
     except ValueError:
         sys.exit("Command-line argument not a number")
