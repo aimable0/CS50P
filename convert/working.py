090c9de51170d8a8872f654e4ffdde926e378d46
aimable0 <aimablenkurikiyimana1@gmail.com>
2024-12-28 23:27:51 +0200

    24 - Clock-formatter

--- convert/working.py
+++ convert/working.py
@@ -1,5 +1,4 @@
 import re
-import sys
 
 
 def main():
@@ -9,23 +8,25 @@ def main():
 def convert(s):
     try:
         match = re.search(r"(\d+):?(\d+)? ([A-Z]{2}) to (\d+):?(\d+)? ([A-Z]{2})", s)
+        # hours assignments.
         if match.group(3) == "AM":
             h1 = match.group(1) if match.group(1) != "12" else "00"
             h2 = str(int(match.group(4)) + 12) if match.group(4) != "12" else "12"
         else:
             h2 = match.group(4) if match.group(4) != "12" else "00"
             h1 = str(int(match.group(1)) + 12) if match.group(1) != "12" else "12"
-            print(h2, h1)
+        # minutes assignments
         min1 = match.group(2) if match.group(2) != None else "00"
         min2 = match.group(5) if match.group(5) != None else "00"
+        # checking for valid time and formatting the returned time.
         if int(h1) <= 12 and int(h2) <= 24 and int(min1) <= 59 and int(min2) <= 59:
-            return f"{h1:02}:{min1} to {h2}:{min2}"
+            return f"{int(h1):02}:{min1} to {h2}:{min2}"
         elif int(h1) <= 24 and int(h2) <= 12 and int(min1) <= 59 and int(min2) <= 59:
-            return f"{h1:02}:{min1} to {int(h2):02}:{min2}"
+            return f"{h1}:{min1} to {int(h2):02}:{min2}"
         else:
             raise ValueError
-    except (AttributeError, ValueError):
-        sys.exit("Invalid format")
+    except AttributeError:
+        raise ValueError
 
 
 if __name__ == "__main__":
--- test.py
+++ test.py
@@ -1,2 +1,7 @@
-amount = 110245015.402856970
-print(f"{amount:,.4f}")
+import re
+
+text = "I usually love dont people"
+# matches = re.search(r"(I)? usually (love)? (dont) (people)", text)
+# print(len(matches.groups()))
+amount = 20
+print(f"{amount:02}")
